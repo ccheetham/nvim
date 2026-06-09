2dfd3532cfa4ce4d00482df5a754ff5d34520574
Chris Cheetham <chris@cheetham.com>
2026-06-09 08:19:48 -0400

    add configuration keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,7 @@
 -- quit
 vim.keymap.set('n', '<leader>q', ':quit<cr>')
 vim.keymap.set('n', '<leader>Q', ':quitall!<cr>')
+
+-- configuration
+vim.keymap.set('n', '<leader>ec', ':edit $MYVIMRC<cr>')
+vim.keymap.set('n', '<leader>sc', ':source $MYVIMRC<cr>')
