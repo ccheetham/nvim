9d83dc4203fa785f5084054eff196724bd59ecde
Chris Cheetham <chris@cheetham.com>
2026-06-24 11:00:20 -0400

    fix: blink.cmp needs blink_cmp_fuzzy

--- lua/20plugins/blink.lua
+++ lua/20plugins/blink.lua
@@ -1,1 +1,9 @@
 vim.pack.add { { src = GitRepo 'saghen/blink.cmp', version = vim.version.range '1.*' } }
+
+require('blink.cmp').setup {
+  fuzzy = {
+    prebuilt_binaries = {
+      download = true,
+    },
+  },
+}
