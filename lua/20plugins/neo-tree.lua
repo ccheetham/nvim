93b95245e6329a69dc1aa63e1c25c85181c69063
Chris Cheetham <chris@cheetham.com>
2026-06-24 11:01:02 -0400

    fix: neo-tree depends on plenary

--- lua/20plugins/neo-tree.lua
+++ lua/20plugins/neo-tree.lua
@@ -1,7 +1,11 @@
-vim.pack.add {
-  { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
-  'https://github.com/MunifTanjim/nui.nvim',
+---@type (string|vim.pack.Spec)[]
+local neotree_plugins = {
+  { src = GitRepo 'nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
+  GitRepo 'nvim-lua/plenary.nvim',
+  GitRepo 'MunifTanjim/nui.nvim',
 }
+
+vim.pack.add(neotree_plugins)
 
 vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
 
