1164d90998a4ae456e051bb6d40ed65175de3c22
Chris Cheetham <chris@cheetham.com>
2026-06-27 09:39:09 -0400

    feat: use catppuccin color themes

---
+++ lua/20plugins/catppuccin.lua
@@ -0,0 +1,3 @@
+vim.pack.add { GitRepo 'catppuccin/nvim' }
+require('catppuccin').setup()
+vim.cmd.colorscheme 'catppuccin-frappe'
--- lua/20plugins/tokyonight.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add { GitRepo 'folke/tokyonight.nvim' }
-
----@diagnostic disable-next-line: missing-fields
-require('tokyonight').setup {
-  styles = {
-    comments = { italic = false }, -- Disable italics in comments
-  },
-}
-vim.cmd.colorscheme 'tokyonight-night'
